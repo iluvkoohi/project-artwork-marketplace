612323b72dc66a03f465c5153d9e7750cb5bc00e
Kolya Madridano <58349866+iluvkoohi@users.noreply.github.com>
2022-09-08 16:16:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,14 @@ try {
     .connect(process.env.CONNECTION_STRING)
     .then(() => console.log("SERVER IS CONNECTED"))
     .catch(() => console.log("SERVER CANNOT CONNECT"));
-
-  app.use(cors({ origin: true, credentials: true}));
+  
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    next();
+  });
+  app.use(cors());
   app.use(cookieParser());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
