a401a44e4a263aaceae30cbfd2d49d3a54e4f75d
Kolya <58349866+iluvkoohi@users.noreply.github.com>
2022-10-17 17:06:30 +0800

    Add express limit

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ try {
   }));
 
   app.use(cookieParser());
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ limit: '25mb', extended: true }));
   app.use(multer({ storage, fileFilter }).array("media"));
 
   app.use("/api", require("./routes/routeUser"));
